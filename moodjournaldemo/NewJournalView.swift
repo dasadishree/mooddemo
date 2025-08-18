0455af4f66e86e449e5a5d64ccb0294a1cd37808
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2025-08-18 18:23:38 -0400

    Update NewJournalView.swift comments

--- moodjournaldemo/NewJournalView.swift
+++ moodjournaldemo/NewJournalView.swift
@@ -9,28 +9,35 @@ import SwiftUI
 import SwiftData
 
 struct NewJournalView: View {
+    // load in variables & data
     @Environment(\.modelContext) private var context
     @State private var title: String = ""
     @State private var entry: String = ""
     
     var body: some View {
         NavigationStack {
+            // navbar
             Navbar()
                 .navigationBarBackButtonHidden(true)
+
+            // form for new journals
             VStack{
                 Text("New Journal Entry")
                     .font(.title)
                     .bold()
                     .padding(.bottom, 10)
                     .foregroundStyle(.pink)
-                
+
+                // title
                 TextField("Journal Entry Title", text: $title)
                     .textFieldStyle(.roundedBorder)
-                
+
+                // entry
                 TextField("Journal entry goes here...", text: $entry)
                     .frame(height: 200)
                     .textFieldStyle(.roundedBorder)
-                    
+
+                // button & saving functionality
                 Button(action: {
                     let newEntry = JournalEntryModel(
                         title: title,
@@ -53,8 +60,10 @@ struct NewJournalView: View {
             .padding()
             
             Spacer()
-            
+
+            // links to jounrals and home page
             HStack {
+                    // past journals view
                 NavigationLink(destination: JournalView()) {
                     VStack {
                         Image(systemName: "book.pages.fill")
@@ -68,6 +77,8 @@ struct NewJournalView: View {
                     )
                     .foregroundStyle(Color(red: 235/255, green: 178/255, blue: 210/255))
                 }
+
+                // home view
                 NavigationLink(destination: ContentView()) {
                     VStack {
                         Image(systemName: "house.fill")
